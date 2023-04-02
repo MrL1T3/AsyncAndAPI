0974ef0ec5d05dbc958dbdf4dcd3910c26611453
Emil Anton <emilbstn@gmail.com>
2023-04-02 15:57:36 -0700

    collapse else if statements to take less space

--- src/convert.js
+++ src/convert.js
@@ -7,20 +7,15 @@ export default class conversionHandler {
       const apiResponse = JSON.parse(this.responseText)
       if (this.status === 200) {
         handleResponse(apiResponse.conversion_rate, currency);
-      }
-      else if (this.status === 400){
+      }else if (this.status === 400){
         document.querySelector('#api-error').innerText = "400 error, bad API request"
-      }
-      else if (this.status === 401){
+      }else if (this.status === 401){
         document.querySelector('#api-error').innerText = "401 error, unauthorized access"
-      }
-      else if (this.status === 403){
+      }else if (this.status === 403){
         document.querySelector('#api-error').innerText = "403 error, forbidden to access"
-      }
-      else if (this.status === 404){
+      }else if (this.status === 404){
         document.querySelector('#api-error').innerText = "400 error, resource cannot be found"
-      }
-      else if (this.status === 500){
+      }else if (this.status === 500){
         document.querySelector('#api-error').innerText = "500 error, internal server issue"
       }
     });
